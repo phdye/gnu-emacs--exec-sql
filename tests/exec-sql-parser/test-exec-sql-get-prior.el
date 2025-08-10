93d60c366843ca5ac935ab6c1113844bd8b5b541
phdye <phdye@acm.org>
2025-08-09 20:51:36 -0400

    refactor: split parser tests

--- tests/exec-sql-parser/test-exec-sql-get-prior.el
+++ tests/exec-sql-parser/test-exec-sql-get-prior.el
@@ -10,14 +10,6 @@
       (should (equal (plist-get info :start) '(4 . 0)))
       (should (equal (plist-get info :end) '(4 . 15))))))
 
-(ert-deftest exec-sql-goto-prior-move ()
-  (with-temp-buffer
-    (insert "int a;\nEXEC SQL SELECT * FROM dual;\nint b;\nEXEC SQL COMMIT;\nint c;\n")
-    (goto-char (point-max))
-    (exec-sql-goto-prior)
-    (should (= (line-number-at-pos (point)) 4))
-    (should (= (current-column) 0))))
-
 (ert-deftest exec-sql-get-prior-skip-comments ()
   (with-temp-buffer
     (insert "/*\nEXEC SQL SELECT * FROM dual;\n*/\nEXEC SQL COMMIT;\n")
@@ -25,3 +17,5 @@
     (let ((info (exec-sql-get-prior)))
       (should info)
       (should (equal (plist-get info :start) '(4 . 0))))))
+
+(provide 'test-exec-sql-get-prior)
