de43bd1ec8018e7ec5f8fdc70d4425452ce6b994
philip-h-dye <philip.h.dye@gmail.com>
2025-08-09 21:49:16 -0400

    exec-sql.el : add key bindings for all major entry points

--- exec-sql.el
+++ exec-sql.el
@@ -1,10 +1,29 @@
-;;; exec-sql.el
-
+;;;
 
 (require 'exec-sql-parser)
 
 (add-hook 'c-mode-hook
-          (lambda ()
-            (local-set-key (kbd "C-c C-n") #'execl-sql-goto-next)))
+    (lambda ()
+        (local-set-key (kbd "C-c C-n") #'execl-sql-goto-next)))
+
+(add-hook 'c-mode-hook
+    (lambda ()
+        (local-set-key (kbd "C-c C-p") #'execl-sql-goto-prior)))
+
+;;;
+
+(require 'exec-sql-format)
+
+(add-hook 'c-mode-hook
+    (lambda ()
+        (local-set-key (kbd "C-c C-f") #'exec-sql-format)))
+
+(add-hook 'c-mode-hook
+    (lambda ()
+        (local-set-key (kbd "C-c C-e") #'exec-sql-format-next-block)))
+
+(add-hook 'c-mode-hook
+    (lambda ()
+        (local-set-key (kbd "C-c C-a") #'exec-sql-format-all-blocks)))
 
 ;;;
