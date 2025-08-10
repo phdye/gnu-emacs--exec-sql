6f111e653c34580e73b0de8e0e1d05285a0782ea
philip-h-dye <philip.h.dye@gmail.com>
2025-08-10 07:51:14 -0400

    exec-sql-init.el : revamped

--- exec-sql-init.el
+++ exec-sql-init.el
@@ -5,17 +5,23 @@
 (add-to-list 'auto-mode-alist '("\\.ec\\'"  . c-mode))
 (add-to-list 'auto-mode-alist '("\\.sqc\\'" . c-mode))
 
+(require 'exec-sql-mmm)
+
 ;;;
 
 (require 'exec-sql-parser)
 
 (add-hook 'c-mode-hook
     (lambda ()
-        (local-set-key (kbd "C-c C-n") #'execl-sql-goto-next)))
+        (local-set-key (kbd "C-c C-n") #'exec-sql-goto-next)))
 
 (add-hook 'c-mode-hook
     (lambda ()
-        (local-set-key (kbd "C-c C-p") #'execl-sql-goto-prior)))
+        (local-set-key (kbd "C-c C-p") #'exec-sql-goto-prior)))
+
+(add-hook 'c-mode-hook
+    (lambda ()
+        (local-set-key (kbd "C-c C-e") #'exec-sql-extract)))
 
 ;;;
 
@@ -23,14 +29,14 @@
 
 (add-hook 'c-mode-hook
     (lambda ()
-        (local-set-key (kbd "C-c C-f") #'exec-sql-format)))
+        (local-set-key (kbd "M-f C-f") #'exec-sql-format)))
 
 (add-hook 'c-mode-hook
     (lambda ()
-        (local-set-key (kbd "C-c C-e") #'exec-sql-format-next-block)))
+        (local-set-key (kbd "M-f C-n") #'exec-sql-format-next-block)))
 
 (add-hook 'c-mode-hook
     (lambda ()
-        (local-set-key (kbd "C-c C-a") #'exec-sql-format-all-blocks)))
+        (local-set-key (kbd "M-f C-a") #'exec-sql-format-all-blocks)))
 
 ;;;
